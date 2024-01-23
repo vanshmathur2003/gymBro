747890ba34cc9f68fa32d1ffb77fb461d794b68f
vanshmathur2003 <vanshmathur26@gmail.com>
2024-01-23 21:49:37 +0530

    Api functionality added
    Main Brain Done, Need work on Css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SearchExercise from "./components/SearchExercise";
+
+function App() {
+  return (
+    <div>
+      <h1>Your Fitness App</h1>
+      <SearchExercise />
+
+    </div>
+  );
+}
+
+export default App;
