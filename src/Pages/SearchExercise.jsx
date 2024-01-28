ae271d08607d94dbcae977a7cd93a8348a73b294
vanshmathur2003 <vanshmathur26@gmail.com>
2024-01-28 14:45:26 +0530

    Update SearchExercise.jsx

--- src/Pages/SearchExercise.jsx
+++ src/Pages/SearchExercise.jsx
@@ -6,6 +6,7 @@ import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+
 const SearchExercise = () => {
   const [exerciseList, setExerciseList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
